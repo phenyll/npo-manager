de4a008ebf60b34fdb433c05c43bd06f5d2bcc20
Oliver Möller <om@batix.com>
2025-03-09 22:38:39 +0100

    Remove composite primary key from user-role mapping table

--- server/db.js
+++ server/db.js
@@ -72,7 +72,7 @@ const db = new sqlite3.Database(dbPath, (err) => {
                 role_id INTEGER NOT NULL,
                 FOREIGN KEY (user_id) REFERENCES users (id),
                 FOREIGN KEY (role_id) REFERENCES roles (id),
-                PRIMARY KEY (user_id, role_id)
+                PRIMARY KEY (user_id)
             )
         `);
         db.run(`
